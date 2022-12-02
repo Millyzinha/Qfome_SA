bb643796f5875e5bbb6319255b8a82302e7d6e4f
dai222j <rstaffk@gmail.com>
2022-12-02 14:18:36 -0300

    home e footer

--- src/Components/HomeDeslogado/homeDeslogado.js
+++ src/Components/HomeDeslogado/homeDeslogado.js
@@ -6,6 +6,12 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import LogoHamburguer from "../HomeDeslogado/imagens/logohamb.png"
 
+import { BsFillCartCheckFill } from "react-icons/bs"
+  import { GiFoodTruck } from "react-icons/gi"
+  import { RiTeamLine, RiHandHeartFill } from "react-icons/ri"
+    import{ IoIosCart } from "react-icons/io"
+    import {AiOutlineTeam} from "react-icons/ai"
+  
 
 const HOMEDeslogado = () => {
 
@@ -30,48 +36,25 @@ const HOMEDeslogado = () => {
 </div>
         </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-{/* GUSTAVO, MEXER SÓ NO QUE ESTA DENTRO DA DIV MEIO */}
-
-{/* PARA ACESSAR A PAGINA: http://localhost:3000/HomeDeslogado */}
-
-
-
-
-
-
-
-
         <div className='meio'>
 
             <div className='divflex'>
         <div className='marklandingtext'>
-
+            <div className='daiana'>
             <h2 className='LandMARK'>Encontre a refeição perfeita </h2>
             <h2 className='LandMARK'>para qualquer hora do seu dia, </h2>
             <h2 className='LandMARKF'>cadastre-se na Qfome e faça Login!</h2>
 </div>
 <div className='buttons'>
 
-<a
+{/* <a
           href={  "/login"}
           target="_blank"
           className="button"
           rel="noreferrer"
         >
           Logar
-        </a>
+        </a> */}
 
         <a
           href={  "/cadastro"}
@@ -79,33 +62,88 @@ const HOMEDeslogado = () => {
           className="button"
           rel="noreferrer"
         >
-          Cadastrar
+          COMEÇAR
         </a>
-
         </div>
         </div>
-  
-            <div className='hamb'>
+        <div className='hamb'>
                     <img src={LogoHamburguer} alt="LogoLANDING" className='LOGOHAMB'/>
                 </div>
         </div>
+  
+           
+        </div>
 
 
+{/* <div className='icons'>
+
+<div className='div1'>
+  <div className='teste11'>
+  < IoIosCart className='carrinhook'/> 
+  <p className='CSS'>Compra simples e segura</p>
+  </div>
+
+  <GiFoodTruck className='caminhao'/>
+  <p className='ERR'>Entrega rápida e rastreada</p>
+</div>
 
 
+<div className='div2'>
+  <RiHandHeartFill className='mao'/> 
+  <p className='REI'>Refeições feitas com amor</p>
 
 
-{/* FIM DA DIV MEIO  */}
+  <AiOutlineTeam className='team' />
+  <p className='EAA'>Equipe atenciosa</p>
 
+</div>
+</div> */}
 
+<div className="container-Comof">
+            <div className="content">
+              <div className="image">
+                < IoIosCart className='carrinhook'/>
+              </div>
+              <p className="description">
+               Compra simples e segura
+              </p>
 
+            </div>
+            <div className="content">
+              <div className="image">
+                <GiFoodTruck className='carrinhook'/>
+              </div>
+              <p className="description">
+               Entrega rápida e rastreada
+              </p>
 
+            </div>
+            <div className="content">
+              <div className="image">
+                 <RiHandHeartFill className='carrinhook'/> 
+              </div>
+              <p className="description">
+                Refeições feitas com amor
+              </p>
+
+            </div>
+            <div className="content">
+              <div className="image">
+                <AiOutlineTeam className='carrinhook' />
+              </div>
+              <p className="description">
+                Equipe atenciosa
+              </p>
+
+            </div>
+          </div>
 
 
         <div className='footerdiv'>
             < Footer />
             </div>
-            </div>   
+            </div>  
+             
     )
 }    
-export default HOMEDeslogado 
+export default HOMEDeslogado
